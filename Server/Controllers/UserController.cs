b8610e9fb30bbe3e2b4c3be4e181207d17c95c3e
Eoic <strazdas.karolis@outlook.com>
2019-09-24 17:02:26 +0300

    Add client project boilerplate

---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Server.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*", exposedHeaders: "X-My-Header")]
+    public class UserController : ApiController
+    {
+        // GET: api/User
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/User/5
+        public string Get(int id)
+        {
+            System.Diagnostics.Debug.WriteLine("HTTP GET");
+            return "value";
+        }
+
+        // POST: api/User
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
