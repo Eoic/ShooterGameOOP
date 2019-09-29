9212b1de51df6613a9705bd8e960d0c663194d7f
Eoic <strazdas.karolis@outlook.com>
2019-09-30 00:49:49 +0300

    Remove redundant code

--- Server/Global.asax.cs
+++ Server/Global.asax.cs
@@ -14,7 +14,6 @@ namespace Server
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            new Loop().Start();
         }
     }
 }
--- Server/Network/MessageType.cs
+++ Server/Network/MessageType.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Server.Network
+﻿namespace Server.Network
 {
     // Event types received from client 
     // through web socket connection
