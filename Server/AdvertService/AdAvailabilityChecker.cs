64dc889dec9816f87c6931f49b2f4475bb70edfd
lukasberulis <lukas.berulis@ktu.edu>
2019-12-08 23:22:31 +0200

    Added Chain of command

---
+++ Server/AdvertService/AdAvailabilityChecker.cs
@@ -0,0 +1,32 @@
+﻿using Server.AdvertSerivce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server.AdvertService
+{
+    public class AdAvailabilityChecker : IAdProvider
+    {
+        private const int MinUserAge = 18;
+        private IAdProvider AdProvider;
+        private int UserAge;
+      
+
+        public AdAvailabilityChecker(IAdProvider adProvider, int userAge)
+        {
+            AdProvider = adProvider;
+            UserAge = userAge;
+        }
+
+        public string GetAd()
+        {
+            if (UserAge >= MinUserAge)
+            {
+                return AdProvider.GetAd();
+            }
+
+            return "";
+        }
+    }
+}
